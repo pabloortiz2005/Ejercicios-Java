33292e4b95e10efc549c893feec5f9e861816600
Pablo Ortiz Muñoz <pabloortiz22.04.2005@gmail.com>
2024-02-29 17:13:04 +0100

    a

--- Programacion/InterfazSupermercado/src/supermercado/App.java
+++ Programacion/InterfazSupermercado/src/supermercado/App.java
@@ -10,7 +10,7 @@ public class App {
 	public static void main(String[] args) {
 
 
-
+//hol
 
 		ArrayList<InterfazAlimento> Alimentos = new ArrayList<>();
 		ArrayList<InterfazLiquido> liquidos = new ArrayList<>();
